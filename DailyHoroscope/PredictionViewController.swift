bc6303f74b669da729ad7e37fd5037dcfd58477a
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2021-03-02 22:44:43 +0500

    Addition CollViewCont-r, NavCont-r, ViewCont-r. Addition files for these contr-s. Addition UILabels and UiImages and IBOutlets for them. Addition file for CollCell and configuring cell.

---
+++ DailyHoroscope/PredictionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PredictionViewController.swift
+//  DailyHoroscope
+//
+//  Created by Александр on 02.03.2021.
+//
+
+import UIKit
+
+class PredictionViewController: UIViewController {
+    @IBOutlet var predictionLabel: UILabel!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var predictionImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DailyHoroscope/ZodiacalCell.swift
@@ -0,0 +1,15 @@
+//
+//  ZodiacalCell.swift
+//  DailyHoroscope
+//
+//  Created by Александр on 02.03.2021.
+//
+
+import UIKit
+
+class ZodiacalCell: UICollectionViewCell {
+    @IBOutlet var zodiacNameLabel: UILabel!
+    @IBOutlet var zodiacPeriodLabel: UILabel!
+    @IBOutlet var zodiacImageView: UIImageView!
+    
+}
